8623d79a8a49a3b3129ff29848ef7063b7bd45b9
Shruti <er.shrutimishra.2002@gmail.com>
2024-01-10 18:45:26 +0530

    done

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 // const MONGO_URL = "mongodb+srv://pawnalakeholiday:%23PawnaLake123@pawnalake.apaxdtd.mongodb.net/?retryWrites=true&w=majority"
 const MONGO_URL = "mongodb://127.0.0.1/Uniqserve_db"
+const HOSTED_MONGO_URL = "mongodb+srv://Uniqueserve:Uniquemongodbpass%40123@cluster0.ce0pqw5.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect(MONGO_URL);
+mongoose.connect(HOSTED_MONGO_URL);
 
 const db = mongoose.connection;
 
